3891fc0a98c7e30bd2c64186eb1821510768b3a6
xuanlin129 <xuan.lin129@gmail.com>
2025-08-25 17:17:36 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my-gatsby-project`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
